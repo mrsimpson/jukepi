9fba1f29a43c63e6446bf95849a5e9b3e6a69a0b
Oliver Jägle <github@beimir.net>
2018-08-10 08:30:10 +0200

    Use list instead of map for buttons

--- buttons/SimplePushbuttons.py
+++ buttons/SimplePushbuttons.py
@@ -6,31 +6,32 @@ import os
 from subprocess import call
 from functools import partial
 
-BUTTONS = {}
+BUTTONS = []
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD) #we're referring to the pin numbers in order, not the GPIO IDs
 
 # Map pins to functions
 button = {'pin': 14, 'command': 'if volumio status | grep -q play; then volumio pause; else volumio play; fi'}
-BUTTONS[button.pin] = button
+BUTTONS.append(button)
 
 button = {'pin': 13, 'command': 'volumio next'}
-BUTTONS[button.pin] = button
+BUTTONS.append(button)
 
 button = {'pin': 11, 'command': 'volumio next'}
-BUTTONS[button.pin] = button
+BUTTONS.append(button)
 
 # We're using normal pushbuttons - if you use a rotary encoder, check the native volumio-plugin
 button = {'pin': 31, 'command': 'volumio volume plus'}
-BUTTONS[button.pin] = button
+BUTTONS.append(button)
 
 button = {'pin': 29, 'command': 'volumio volume minus'}
-BUTTONS[button.pin] = button
+BUTTONS.append(button)
 
 def executeOs(command):
     os.system(button.command)
 
-for pin, command in BUTTONS:
-    GPIO.add_event_detect(pin, GPIO.RISING)
-    GPIO.add_event_callback(pin, partial(executeOs, command))
+for button in BUTTONS:
+    GPIO.setup(button.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(button.pin, GPIO.RISING)
+    GPIO.add_event_callback(button.pin, partial(executeOs, button.command))
